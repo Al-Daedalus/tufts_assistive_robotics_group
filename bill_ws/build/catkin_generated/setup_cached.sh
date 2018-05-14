cbfc91c3978068e78de4a1477a3132003b236305
Alphonsus Adu-Bredu <alphonsusbq436@gmail.com>
2018-05-14 15:20:15 -0400

    teleop_joy fully functional

--- bill_ws/build/catkin_generated/setup_cached.sh
+++ bill_ws/build/catkin_generated/setup_cached.sh
@@ -8,6 +8,9 @@
 
 # modified environment variables
 export CMAKE_PREFIX_PATH="/home/bill/bill_ros/bill_ws/devel:$CMAKE_PREFIX_PATH"
+export LD_LIBRARY_PATH="/home/bill/bill_ros/bill_ws/devel/lib:$LD_LIBRARY_PATH"
+export PATH="/opt/ros/kinetic/bin:/home/bill/bin:/home/bill/.local/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games:/usr/local/games:/snap/bin"
+export PKG_CONFIG_PATH="/home/bill/bill_ros/bill_ws/devel/lib/pkgconfig:$PKG_CONFIG_PATH"
 export PWD="/home/bill/bill_ros/bill_ws/build"
 export ROSLISP_PACKAGE_DIRECTORIES="/home/bill/bill_ros/bill_ws/devel/share/common-lisp"
 export ROS_PACKAGE_PATH="/home/bill/bill_ros/bill_ws/src:$ROS_PACKAGE_PATH"
--- bill_ws/build/cmake_install.cmake
+++ bill_ws/build/cmake_install.cmake
@@ -117,6 +117,7 @@ endif()
 if(NOT CMAKE_INSTALL_LOCAL_ONLY)
   # Include the install script for each subdirectory.
   include("/home/bill/bill_ros/bill_ws/build/gtest/cmake_install.cmake")
+  include("/home/bill/bill_ros/bill_ws/build/bill_teleop_joy/cmake_install.cmake")
 
 endif()
 
